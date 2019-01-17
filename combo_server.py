3115b5d8b33cbc0aefddc43fb7c6e4ea671f5874
AnniePkida <madortwo@gmail.com>
2019-01-17 13:37:20 +0200

    modified server to use flask framework

--- combo_server.py
+++ combo_server.py
@@ -8,15 +8,12 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'contrib')))
 
 from server_source_locator import *
+from flask import Flask, request
+app = Flask(__name__)
 
-
-MAX_REQUEST_LENGTH = 256
-ACK_MSG = struct.pack('>i', 0xF00BA4)
-NACK_MSG = struct.pack('>i', 0xDEC11E)
 SOURCES_JSON = 'sources.json'
 
 source_locator = ServerSourceLocator(SOURCES_JSON)
-
 
 def full_json():
     d = {
@@ -36,59 +33,36 @@ def full_json():
 
     return json.dumps(d)
 
+@app.route("/", methods=['POST', 'GET'])
+def handle_client_request():
+    REQUEST_TYPE_GET_SOURCE = "get_source"
+    if request.method == 'POST':
+        # Todo: handle post requests
+        return "Post method is yet to be developed..."
 
-def handle_client_connection(client_socket):
-    try:
-        request_length = client_socket.recv(4)
+    elif request.method == 'GET':
+        #handling GET requests
+        try:
+            request_type = request.args.get('request_type')
+            assert request_type != None, "No request type"
 
-        unpacked_length = struct.unpack('>i', request_length)[0]
-        assert unpacked_length <= MAX_REQUEST_LENGTH,\
-            'Unsupported request length (received {}, max is {})'.format(unpacked_length, MAX_REQUEST_LENGTH)
-        client_socket.send(ACK_MSG)
+            if request_type == REQUEST_TYPE_GET_SOURCE:
+                project_name = request.args.get('project_name')
+                assert project_name != None, "No project name"
 
-        request = client_socket.recv(unpacked_length)
-        assert len(request) == unpacked_length,\
-            'Received {} bytes of request instead of {}'.format(len(request), unpacked_length)
+                project_version = request.args.get('project_version')
+                assert project_version != None, "No project version"
 
-        request_tuple = tuple(request.decode().split(';'))
-        assert len(request_tuple) == 2, 'Expected a request with project name and version number'
+                VersionNumber(project_version) # Make sure version is valid
 
-        project_name, version = request_tuple
-        VersionNumber(version)  # Make sure version is valid
-
-        source = source_locator.get_source(project_name, version)
-        client_socket.send(source.as_dict().encode())
-
-    except BaseException as e:
-        client_socket.send(NACK_MSG)
-        print('Failed to handle request from', client_socket)
-        print(e, '\n')
-
-    client_socket.close()
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Combo Server Arguments')
-    parser.add_argument('port', help='TCP Port', type=int)
-    parser.add_argument('sources_json', help='A sources json')
-    args = parser.parse_args()
-
-    address = ('0.0.0.0', args.port)
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(address)
-    server.listen(5)  # max backlog of connections
-
-    print('Listening on {}:{}'.format(*address))
-
-    while True:
-        client_sock, address = server.accept()
-        print('Accepted connection from {}:{}'.format(*address))
-        client_handler = threading.Thread(
-            target=handle_client_connection,
-            args=(client_sock,)
-        )
-        client_handler.start()
-
-
+                source = source_locator.get_source(project_name, project_version)
+                return source.as_dict().encode()
+            else:
+                return "Request type is not yet supported..."
+        except BaseException as e:
+            print('Failed to handle request')
+            return "error : "+ str(e)
+    
 if __name__ == '__main__':
-    main()
+    #main()
+    app.run()
