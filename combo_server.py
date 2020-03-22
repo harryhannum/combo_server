d2ec24447e6ee064e71e449dd0bba7404e07ea8c
Harry Hannum <harryhannum@gmail.com>
2020-03-22 16:27:24 +0200

    United requests error handling to a single function

--- combo_server.py
+++ combo_server.py
@@ -12,6 +12,14 @@ app = Flask(__name__)
 DEFAULT_INDEXER_JSON = 'sources_index.json'
 
 
+def handle_request(handling_func, *args):
+    try:
+        return handling_func(*args)
+    except BaseException as e:
+        print('Failed to handle request')
+        return 'Error (type "{}"): {}'.format(type(e).__name__, e)
+
+
 @app.route('/get_available_versions', methods=['GET'])
 def get_available_versions_request():
     # TODO: Get actual dictionary
@@ -35,55 +43,53 @@ def get_available_versions_request():
 
 @app.route('/get_source', methods=['GET'])
 def get_source_request():
-    try:
-        project_name = request.args.get('project_name')
-        assert project_name is not None, 'Missing project name'
+    project_name = request.args.get('project_name')
+    assert project_name is not None, 'Missing project name'
 
-        project_version = request.args.get('project_version')
-        assert project_version is not None, 'Missing project version'
+    project_version = request.args.get('project_version')
+    assert project_version is not None, 'Missing project version'
+
+    def handle():
         VersionNumber.validate(project_version)
 
         source = source_maintainer.get_source(project_name, project_version)
         return json.dumps(source).encode()
-    except BaseException as e:
-        print('Failed to handle request')
-        return 'Error (type {}): {}'.format(type(e), e)
+
+    return handle_request(handle)
 
 
 @app.route('/add_project', methods=['POST'])
 def add_project_request():
-    try:
-        project_name = request.values.get('project_name')
-        assert project_name is not None, 'Missing project name'
+    project_name = request.values.get('project_name')
+    assert project_name is not None, 'Missing project name'
 
-        source_type = request.values.get('source_type')  # Optional
+    source_type = request.values.get('source_type')  # Optional
 
+    def handle():
         source_maintainer.add_project(project_name, source_type)
         return 'Added project "{}" successfully'.format(project_name)
 
-    except BaseException as e:
-        print('Failed to handle request')
-        return 'Error (type {}): {}'.format(type(e), e)
+    return handle_request(handle)
 
 
 @app.route('/add_version', methods=['POST'])
 def add_version_request():
-    try:
-        version_details_str = request.values.get('version_details')
-        assert version_details_str is not None, 'Missing version details'
-        version_details = json.loads(version_details_str)
+    version_details_str = request.values.get('version_details')
+    assert version_details_str is not None, 'Missing version details'
 
-        details = source_maintainer.add_version(version_details)
-        if details:
-            return 'Added details of version "{}" of project "{}" successfully'.format(details.version, details.name)
+    def handle():
+        try:
+            version_details = json.loads(version_details_str)
 
-        return 'Added version details "{}" successfully'.format(version_details)
+            details = source_maintainer.add_version(version_details)
+            if details:
+                return 'Added details of version "{}" of project "{}" successfully'.format(details.version, details.name)
 
-    except RequestedVersionAlreadyExisted as e:
-        return e.message
-    except BaseException as e:
-        print('Failed to handle request')
-        return 'Error (type {}): {}'.format(type(e), e)
+            return 'Added version details "{}" successfully'.format(version_details)
+        except RequestedVersionAlreadyExisted as e:
+            return e.message
+
+    return handle_request(handle)
 
 
 if __name__ == '__main__':
